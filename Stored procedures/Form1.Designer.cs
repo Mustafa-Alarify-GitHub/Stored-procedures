88e341910561cc2553705b9c554fe9618db78871
Mustafa Al-arify <m2024u775516215@gmail.com>
2024-06-17 18:10:00 +0300

    Add project files.

---
+++ Stored procedures/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Stored_procedures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(78, 54);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(206, 20);
+            this.txt_name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "price";
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(78, 91);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(206, 20);
+            this.txt_price.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(97, 138);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(54, 35);
+            this.btn_Add.TabIndex = 5;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(157, 138);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(54, 35);
+            this.btn_Delete.TabIndex = 6;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(217, 138);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(54, 35);
+            this.btn_update.TabIndex = 7;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_name);
+            this.groupBox1.Controls.Add(this.btn_update);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_Delete);
+            this.groupBox1.Controls.Add(this.txt_price);
+            this.groupBox1.Controls.Add(this.btn_Add);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 190);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Text Filed";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Shose item"});
+            this.comboBox1.Location = new System.Drawing.Point(97, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(174, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ID";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(-1, 208);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(338, 271);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Table Data";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.SandyBrown;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 8F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(332, 252);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(337, 484);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Stored procedures/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Stored_procedures
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"server=DESKTOP-DA5JGBQ\SQLEXPRESS; DataBase=TB_Test; Integrated Security =true");
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable dataTable = new DataTable();
+
+        SqlDataAdapter adapter2;
+        DataTable dataTable2 = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllData();
+        }
+
+     
+
+        private void GetAllData()
+        {
+            try
+            {
+                dataTable.Clear();
+                con.Open();
+                cmd = new SqlCommand("GetAllData", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+
+                // ComboBox
+                comboBox1.DataSource = dataTable;
+
+                comboBox1.DisplayMember = "Name";
+                comboBox1.ValueMember = "id";
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // Event New
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_name.Text = String.Empty;
+            txt_price.Text = String.Empty;
+         }
+
+        // Event Insert
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd = new SqlCommand("AddNewData", con);
+                cmd.CommandType= CommandType.StoredProcedure;
+                SqlParameter[] param = new SqlParameter[2];
+           
+
+                // var name
+                param[0] = new SqlParameter("@name", SqlDbType.VarChar);
+                param[0].Value = txt_name.Text;
+
+                // var price
+                param[1] = new SqlParameter("@price", SqlDbType.VarChar);
+                param[1].Value = txt_price.Text;
+
+                cmd.Parameters.AddRange(param);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MyMessage("Created");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // Event Delete
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd = new SqlCommand("DeleteData", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter param = new SqlParameter();
+                // var ID
+                param = new SqlParameter("@id", SqlDbType.Int);
+                param.Value = comboBox1.SelectedValue.ToString();
+
+                cmd.Parameters.Add(param);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MyMessage("Deleted");
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // UpdateData
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd = new SqlCommand("UpdateData", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter[] param = new SqlParameter[3];
+
+                // var ID
+                param[0] = new SqlParameter("@id",SqlDbType.Int);
+                param[0].Value= comboBox1.SelectedValue.ToString();
+
+                // var Name
+                param[1] = new SqlParameter("@name",SqlDbType.VarChar);
+                param[1].Value=txt_name.Text;
+
+                // var price
+                param[2] = new SqlParameter("@price",SqlDbType.VarChar);
+                param[2].Value=txt_price.Text;
+
+                cmd.Parameters.AddRange(param);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MyMessage("Updated");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // My Message
+        void MyMessage(string title)
+        {
+            GetAllData();
+            MessageBox.Show($"The {title} is Successfully", $"{title} Data", MessageBoxButtons.OK, MessageBoxIcon.Information);     
+        }
+
+        // Event Get Data By ID
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "Shose item")
+                {
+                    return;
+                }
+                if (con.State == ConnectionState.Closed) {
+                    con.Open();
+                }
+
+                //cmd = new SqlCommand("GetDataById", con);
+                //cmd.CommandType = CommandType.StoredProcedure;
+                //SqlParameter param = new SqlParameter();
+
+                ////var ID
+                //param = new SqlParameter("@id", SqlDbType.Int);
+                //param.Value = comboBox1.SelectedValue.ToString();
+
+                //cmd.Parameters.Add(param);
+                //adapter2 = new SqlDataAdapter("GetDataById", con);
+                //adapter2.Fill(dataTable2);
+                //con.Close();
+
+                //txt_name.Text = dataTable2.Rows[0]["Name"].ToString();
+                //txt_price.Text = dataTable2.Rows[0]["Price"].ToString();
+            }
+            catch (SqlException E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
